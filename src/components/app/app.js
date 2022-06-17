f9843b8d5ec0390de388d65d5e1c5d68b555a71f
Osman Sagsoz <osmansagsoz1@gmail.com>
2022-06-17 19:22:24 +0300

    mobile layout

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,7 +1,7 @@
 import css from "./app.module.css";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faList, faCheckDouble } from '@fortawesome/free-solid-svg-icons';
-import { cx } from 'classnames';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faList, faCheckDouble } from "@fortawesome/free-solid-svg-icons";
+import { cx } from "classnames";
 
 console.log(css);
 export const App = () => {
@@ -17,29 +17,29 @@ export const App = () => {
             <div></div>
             <div></div>
           </div>
-          <FontAwesomeIcon icon={faCheckDouble} />
+          <FontAwesomeIcon icon={faCheckDouble} className={css.icon} />
         </header>
         <div className={css.todoArea}>
           <div className={css.inputField}>
             <form className={css.mainForm} autoComplete="off">
               <textarea className={css.mainInput} placeholder="Meeting with mentor"></textarea>
+              <div className={css.extraField}>
+                <button className={css.dateField}>Today</button>
+                <div className={css.todoTags}>
+                  <button className={css.listTag} aria-label="Add list tag">
+                    Tag
+                  </button>
+                  <button className={css.priority} aria-label="Set priority">
+                    Pri
+                  </button>
+                </div>
+              </div>
             </form>
-          </div>
-          <div className={css.extraField}>
-            <div className={css.dateField}>Today</div>
-            <div className={css.todoTags}>
-              <button className={css.listTag} aria-label="Add list tag">
-                Tag
-              </button>
-              <button className={css.priority} aria-label="Set priority">
-                Pri
-              </button>
-            </div>
           </div>
         </div>
       </section>
-      <FontAwesomeIcon icon={faList} />
       <section className={css.allLists}>
+        <FontAwesomeIcon icon={faList} />
         <h2 className={css.todoListsTitle}>My Lists</h2>
         <ul className={css.todoLists}>
           <li className={css.activeList}>Meetings</li>
@@ -58,62 +58,68 @@ export const App = () => {
           </button>
         </form>
       </section>
-      <section className={css.todoList}>
-        <h2 className={css.todoListTitle}>Meetings</h2>
-        <div className={css.todoListBody}>
-          <ul className={css.todos}>
-            <li className="">
-              <input type="checkbox" />
-              <span className={css.customCheckbox}></span>
-              <label className="">
-                <span className={css.todoSpan}>Meeting with project group</span>
-                <form className={css.editForm}>
-                  <input type="text" className={css.editInput} />
-                </form>
-              </label>
-            </li>
-            <li className="">
-              <input type="checkbox" />
-              <span className={css.customCheckbox}></span>
-              <label className="">
-                <span className={css.todoSpan}>Meeting with manager</span>
-                <form className={css.editForm}>
-                  <input type="text" className={css.editInput} />
-                </form>
-              </label>
-            </li>
-          </ul>
-        </div>
-        <div className={css.deleteArea}>
-          <button className={css.clearCompleted} aria-label="Clear completed todos">
-            Clear completed todos
-          </button>
-          <button className={css.deleteList} aria-label="Delete list">
-            Delete list
-          </button>
+      <section className={css.todoListArea}>
+        <div className={css.todoList}>
+          <h2 className={css.todoListTitle}>Meetings</h2>
+          <div className={css.todoListBody}>
+            <ul className={css.todos}>
+              <li className="">
+                <input type="checkbox" />
+                <span className={css.customCheckbox}></span>
+                <label className="">
+                  <span className={css.todoSpan}>Meeting with project group</span>
+                  <form className={css.editForm}>
+                    <input type="text" className={css.editInput} />
+                  </form>
+                </label>
+              </li>
+              <li className="">
+                <input type="checkbox" />
+                <span className={css.customCheckbox}></span>
+                <label className="">
+                  <span className={css.todoSpan}>Meeting with manager</span>
+                  <form className={css.editForm}>
+                    <input type="text" className={css.editInput} />
+                  </form>
+                </label>
+              </li>
+            </ul>
+          </div>
+          <div className={css.deleteArea}>
+            <button className={css.clearCompleted} aria-label="Clear completed todos">
+              Clear completed todos
+            </button>
+            <button className={css.deleteList} aria-label="Delete list">
+              Delete list
+            </button>
+          </div>
         </div>
       </section>
       <footer>
-      <h3>&copy; Osman Sağsöz 2022</h3>
-        <ul className="foot">
-            <li>
-                <a href="https://www.linkedin.com/in/osman-sagsoz-053621225/" target="_blank" rel="noreferrer">
-                    <span className="fa-brands fa-linkedin-in" aria-hidden="true"></span>
-                    <span className="sr-only">Linked in</span>
-                </a>
-            </li>
-            <li>
-                <a href="https://github.com/osmansagsoz" target="_blank" rel="noreferrer">
-                    <span className="fa-brands fa-github"></span>
-                    <span className="sr-only">Github</span>
-                </a>
-            </li>
-            <li>
-                <a href="https://codepen.io/osmansagsoz" target="_blank" rel="noreferrer">
-                    <span className="fa-brands fa-codepen"></span>
-                    <span className="sr-only">Codepen</span>
-                </a>
-            </li>
+        <h3>&copy; Osman Sağsöz 2022</h3>
+        <ul className={css.foot}>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/osman-sagsoz-053621225/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <span className="fa-brands fa-linkedin-in" aria-hidden="true"></span>
+              <span className="sr-only">Linked in</span>
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/osmansagsoz" target="_blank" rel="noreferrer">
+              <span className="fa-brands fa-github"></span>
+              <span className="sr-only">Github</span>
+            </a>
+          </li>
+          <li>
+            <a href="https://codepen.io/osmansagsoz" target="_blank" rel="noreferrer">
+              <span className="fa-brands fa-codepen"></span>
+              <span className="sr-only">Codepen</span>
+            </a>
+          </li>
         </ul>
       </footer>
     </div>

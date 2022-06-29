28997742b73cce8619e499057d92b167a4db6d30
Osman Sagsoz <osmansagsoz1@gmail.com>
2022-06-29 12:17:27 +0300

    add react router

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,10 +1,8 @@
 import css from "./app.module.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faList, faCheckDouble } from "@fortawesome/free-solid-svg-icons";
-import { faLinkedinIn, faGithub, faCodepen } from '@fortawesome/free-brands-svg-icons'
 import { cx } from "classnames";
 import AllLists from '../allLists/allLists'
 import { Container } from '../container/container'
+import Footer from "../footer/footer";
 
 console.log(css);
 export const App = () => {
@@ -79,30 +77,7 @@ export const App = () => {
           </div>
         </div>
       </section>
-      <footer>
-        <h3>&copy; Osman Sağsöz 2022</h3>
-        <ul className={css.foot}>
-          <li>
-            <a
-              href="https://www.linkedin.com/in/osman-sagsoz-053621225/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <FontAwesomeIcon icon={faLinkedinIn} />
-            </a>
-          </li>
-          <li>
-            <a href="https://github.com/osmansagsoz" target="_blank" rel="noreferrer">
-            <FontAwesomeIcon icon={faGithub} />
-            </a>
-          </li>
-          <li>
-            <a href="https://codepen.io/osmansagsoz" target="_blank" rel="noreferrer">
-            <FontAwesomeIcon icon={faCodepen} />
-            </a>
-          </li>
-        </ul>
-      </footer>
+      <Footer />
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import 'normalize.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { App } from './components/app/app';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <Router>
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function

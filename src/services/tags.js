13ddcd20ae114fdaa8974fc9c4ab1e206ef0d9bf
Rodrigo Rigo <dallrigo@gmail.com>
2022-07-08 12:04:02 -0300

    Use new backend structure

--- src/services/tags.js
+++ src/services/tags.js
@@ -1,11 +1,8 @@
 const tagsUrl = `${process.env.REACT_APP_TODO_API_BASE_URL}/tags`;
 
 export const createTag = async ({ name }) => {
-  // 🚫 Generate ID here, but it should always be in the server!
-  const id = new Date().toISOString();
-
   const result = await fetch(tagsUrl, {
-    body: JSON.stringify({ id, name }),
+    body: JSON.stringify({ name }),
     headers: {
       "Content-Type": "application/json"
     },

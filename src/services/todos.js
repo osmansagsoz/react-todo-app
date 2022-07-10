0152626aa46fb7d81080f1d1f6cbe3d3db37ac45
Osman Sagsoz <osmansagsoz1@gmail.com>
2022-07-10 20:40:24 +0300

    function organization

--- src/services/todos.js
+++ src/services/todos.js
@@ -1,16 +1,19 @@
 const todosUrl = `${process.env.REACT_APP_TODO_API_BASE_URL}/todos`;
 
-export const createTodo = async ({ title, tagId, time }) => {
+export const createTodo = async ({ title, tagId, time, id, completed, priority }) => {
   const result = await fetch(todosUrl, {
     body: JSON.stringify({
-        title,
-        tagId,
-        time: time || Date.now() // unix date!!!
+      id,
+      title,
+      completed: false,
+      tagId,
+      priority,
+      time: time || Date.now() // unix date!!!
     }),
     headers: {
       "Content-Type": "application/json"
     },
-    method: "POST",
+    method: "POST"
   });
   const data = await result.json();
   return data;
@@ -20,4 +23,4 @@ export const getTodos = async (tagId) => {
   const result = await fetch(`${todosUrl}?tagId=${tagId}`);
   const data = await result.json();
   return data;
-}
+};

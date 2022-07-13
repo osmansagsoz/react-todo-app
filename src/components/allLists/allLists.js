ddfbad43ae3b9f2a3119cabcd1d85471c2b73aa4
Osman Sagsoz <osmansagsoz1@gmail.com>
2022-07-13 13:22:48 +0300

    component simplified

--- src/components/allLists/allLists.js
+++ src/components/allLists/allLists.js
@@ -1,11 +1,30 @@
 import css from "./allLists.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faList } from "@fortawesome/free-solid-svg-icons";
-import { ListBody } from "./listBody";
+import ListItem from "./listItem";
+import { Loader } from "../loader/loader";
 import { useDropdown } from "./useDropdown";
+import { useState, useCallback } from "react";
 
 const AllLists = ({ onSelectedTagChange, onTagCreated, selectedTag, tags }) => {
   const [closed, toggleLists] = useDropdown({ initialClosed: true });
+
+  const [newTag, setNewTag] = useState("");
+
+  const onNewTagChange = useCallback((event) => setNewTag(event.target.value), []);
+
+  const handleListSubmit = useCallback(
+    async (event) => {
+      event.preventDefault();
+
+      const newTagName = newTag.trim(); // "  asdas  " => "asdas"
+      if (newTagName) {
+        onTagCreated({ name: newTagName });
+        setNewTag("");
+      }
+    },
+    [newTag, onTagCreated]
+  );
 
   return (
     <section className={css.allLists}>
@@ -13,13 +32,36 @@ const AllLists = ({ onSelectedTagChange, onTagCreated, selectedTag, tags }) => {
         <FontAwesomeIcon icon={faList} />
         <h3 className={css.todoListsTitle}>My Lists</h3>
       </button>
-      <ListBody
-        className={closed ? css.dropdown : css.dropdownOpen}
-        onSelectedTagChange={onSelectedTagChange}
-        onTagCreated={onTagCreated}
-        selectedTag={selectedTag}
-        tags={tags}
-      />
+      <div className={closed ? css.dropdown : css.dropdownOpen}>
+      {tags ? (
+        <ul className={css.todoLists}>
+          {tags.map((tag) => (
+            <ListItem
+              key={tag.id}
+              onSelectedTagChange={onSelectedTagChange}
+              selectedTag={selectedTag}
+              tag={tag}
+            />
+          ))}
+        </ul>
+      ) : (
+        <Loader />
+      )}
+      <form onSubmit={handleListSubmit}>
+        <input
+          type="text"
+          name="listTitle"
+          value={newTag}
+          onChange={onNewTagChange}
+          className={css.newList}
+          placeholder="New list name"
+          aria-label="New list name"
+        />
+        <button className={css.listButton} aria-label="Create new list" type="submit">
+          +
+        </button>
+      </form>
+    </div>
     </section>
   );
 };

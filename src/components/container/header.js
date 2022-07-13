a83f07bdb5703b71040e8d2524563d100cd9f2e2
Osman Sagsoz <osmansagsoz1@gmail.com>
2022-07-13 13:37:31 +0300

    add timeout

--- src/components/container/header.js
+++ src/components/container/header.js
@@ -2,12 +2,30 @@ import { Loader } from "../loader/loader";
 import css from "./container.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCheckDouble } from "@fortawesome/free-solid-svg-icons";
+import { useEffect, useState, useRef } from "react";
 
 export const Header = ({ appState }) => {
+  const [visible, setVisible] = useState(true);
+  const timeOutRef = useRef();
+
+  useEffect(() => {
+    if(appState === "Tags data loaded!") {
+     timeOutRef.current = setTimeout(() => {
+        setVisible(false)
+      }, 3000)
+    } else {
+      setVisible(true);
+    }
+    return () => {
+      if(timeOutRef.current) {
+        clearTimeout(timeOutRef.current);
+      }
+    }
+  }, [appState])
     return (
         <header>
           <h1 className={css.title}>React Todo App</h1>
-          <p className={css.headerP}>{appState}</p>
+          <p className={css.headerP}>{visible ? appState : null}</p>
           {appState === "Loading" ? <Loader /> : null}
           <FontAwesomeIcon icon={faCheckDouble} className={css.icon} />
         </header>
